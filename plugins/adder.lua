7a8f0f57c7b9fc27950beb55a8b7175bda46c93a
mohammad12344 <sdf12345641@gmail.com>
2016-02-28 16:17:46 +0330

    Create adder.lua

---
+++ plugins/adder.lua
@@ -0,0 +1,35 @@
+do
+
+function run(msg, matches)
+local chat_id = ms.to.id
+     chat_add_user('chat#id'..chat_id, 'user#id138438320', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id203758579', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id174555530', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id149108385', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id164218220', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id163546151', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id157012871', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id199808771', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id135296872', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id192579604', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id184147764', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id188351910', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id169609971', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id143355611', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id138013981', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id147756365', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id137240794', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id156747377', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id196044113', ok_cb, true)
+     chat_add_user('chat#id'..chat_id, 'user#id187684772', ok_cb, true)
+
+return {
+  description = "adder only", 
+  usage = "adder : return adder",
+  patterns = {
+    "^[Aa][Dd][Dd][Ee][Rr]$",
+
+  },
+  run = run
+}
+end
