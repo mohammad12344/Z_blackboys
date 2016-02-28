4351952d81430a20ee76d2fcd811a8c23d133327
mohammad12344 <sdf12345641@gmail.com>
2016-02-28 16:40:23 +0330

    Update yid.lua

--- plugins/yid.lua
+++ plugins/yid.lua
@@ -1,7 +1,7 @@
 do
 local function run(msg, matches)
-local bot_id = 180672422
-local fbotmain = 187754586
+local bot_id = 187754586
+local fbotmain = 180672422
 
     if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
